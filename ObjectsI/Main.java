8ae0082ead60a0f40ee1c03026f838f50aa9d5c0
sergi1307 <sergi13072006@gmail.com>
2025-02-10 13:09:53 +0100

    Añadimos más funcionalidades a la clase rectángulo y finalizamos el proyecto 2 de objetos en JAVA

--- ObjectsI/Main.java
+++ ObjectsI/Main.java
@@ -55,60 +55,18 @@ public class Main {
                 System.out.println("Persona 2 es jubilado: " + per1.esJubilado());
 
             } else if (opc == 3) {
-                Rectangulo rec1 = new Rectangulo();
-                Rectangulo rec2 = new Rectangulo();
-                int perimetro1, perimetro2, ancho1, ancho2, altura1, altura2, area1, area2;
+                Rectangulo rec1 = new Rectangulo(0, 0, 5, 5);
+                Rectangulo rec2 = new Rectangulo(7, 9, -3, 7);
 
-                rec1.x1 = 0;
-                rec1.y1 = 0;
-                rec1.x2 = 5;
-                rec1.y2 = 5;
+                rec1.mostrar();
+                rec2.mostrar();
 
-                rec2.x1 = 7;
-                rec2.y1 = 9;
-                rec2.x2 = -3;
-                rec2.y2 = 7;
+                System.out.println("Perímetro del rectángulo 1: " + rec1.perimetro());
+                System.out.println("Área del rectángulo 1: " + rec1.area());
 
-                perimetro1 = rec1.x1 + rec1.y1 + rec1.x2 + rec1.y2;
-                perimetro2 = rec2.x1 + rec2.y1 + rec2.x2 + rec2.y2;
+                System.out.println("Perímetro del rectángulo 2: " + rec2.perimetro());
+                System.out.println("Área del rectángulo 2: " + rec2.area());
 
-                ancho1 = Math.abs(rec1.x1 - rec1.x2);
-                ancho2 = Math.abs(rec2.x1 - rec2.x2);
-
-                altura1 = Math.abs(rec1.y1 - rec1.y2);
-                altura2 = Math.abs(rec2.y1 - rec2.y2);
-
-                area1 = ancho1 * altura1;
-                area2 = ancho2 * altura2;
-
-                System.out.println("Rectángulo 1: Coordenadas ("+rec1.x1+","+rec1.x2+")("+rec1.y1+","+rec1.y2+"). Perímetro: " + perimetro1 +". Area: " + area1 +".");
-                System.out.println("Rectángulo 2: Coordenadas ("+rec2.x1+","+rec2.x2+")("+rec2.y1+","+rec2.y2+"). Perímetro: " + perimetro2 +". Area: " + area2 +".");
-
-                rec1.x1 += 3;
-                rec1.y1 -= 4;
-                rec1.x2 *= 3;
-                rec1.y2 -= 2;
-
-                rec2.x1 += 8;
-                rec2.y1 -= 6;
-                rec2.x2 *= -1;
-                rec2.y2 += 3;
-
-                perimetro1 = rec1.x1 + rec1.y1 + rec1.x2 + rec1.y2;
-                perimetro2 = rec2.x1 + rec2.y1 + rec2.x2 + rec2.y2;
-
-                ancho1 = Math.abs(rec1.x1 - rec1.x2);
-                ancho2 = Math.abs(rec2.x1 - rec2.x2);
-
-                altura1 = Math.abs(rec1.y1 - rec1.y2);
-                altura2 = Math.abs(rec2.y1 - rec2.y2);
-
-                area1 = ancho1 * altura1;
-                area2 = ancho2 * altura2;
-
-                System.out.println("Datos actualizados: ");
-                System.out.println("Rectángulo 1: Coordenadas ("+rec1.x1+","+rec1.x2+")("+rec1.y1+","+rec1.y2+"). Perímetro: " + perimetro1 +". Area: " + area1 +".");
-                System.out.println("Rectángulo 2: Coordenadas ("+rec2.x1+","+rec2.x2+")("+rec2.y1+","+rec2.y2+"). Perímetro: " + perimetro2 +". Area: " + area2 +".");
             } else if (opc != -1) {
                 System.out.println("Valor introducido no correcto.");
             }
--- ObjectsI/Rectangulo.java
+++ ObjectsI/Rectangulo.java
@@ -2,4 +2,28 @@ package ObjectsI;
 
 public class Rectangulo {
     int x1, y1, x2, y2;
+
+    public Rectangulo(int x1, int y1, int x2, int y2) {
+        if (x1 < x2 && y1 < y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        } else {
+            System.out.println("ERROR: Las coordenadas no definen un rectángulo válido.");
+        }
+    }
+    public void mostrar() {
+        System.out.println("Rectángulo [Esquina abajo izquierda: (" + x1 + ", " + y1 + ") - Esquina arriba derecha: (" + x2 + ", " + y2 +")]");
+    }
+    public int perimetro() {
+        int ancho = x2 - x1;
+        int altura = y2 - y1;
+        return 2 * (ancho + altura);
+    }
+    public int area() {
+        int ancho = x2 - x1;
+        int altura = y2 - y1;
+        return ancho * altura;
+    }
 }
